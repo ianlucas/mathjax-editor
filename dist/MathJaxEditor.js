b3e81ef7e1f1de9553c3c3ebc3718ebdc66c4a61
Ian Lucas <ianlucas_ferreira@hotmail.com>
2016-12-25 17:24:53 -0300

    [Build] 1.1.7

--- dist/MathJaxEditor.js
+++ dist/MathJaxEditor.js
@@ -67,13 +67,11 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-	__webpack_require__(1);
-
-	var _Editor = __webpack_require__(5);
+	var _Editor = __webpack_require__(1);
 
 	var _Editor2 = _interopRequireDefault(_Editor);
 
-	var _extendMathJax = __webpack_require__(10);
+	var _extendMathJax = __webpack_require__(6);
 
 	var _extendMathJax2 = _interopRequireDefault(_extendMathJax);
 
@@ -101,7 +99,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	    var editor = new _Editor2.default(options);
 
 	    this.editor = editor;
-	    this.version = '1.1.6';
+	    this.version = '1.1.7';
 	  }
 
 	  /**
@@ -252,354 +250,6 @@ return /******/ (function(modules) { // webpackBootstrap
 /* 1 */
 /***/ function(module, exports, __webpack_require__) {
 
-	// style-loader: Adds some css to the DOM by adding a <style> tag
-
-	// load the styles
-	var content = __webpack_require__(2);
-	if(typeof content === 'string') content = [[module.id, content, '']];
-	// add the styles to the DOM
-	var update = __webpack_require__(4)(content, {});
-	if(content.locals) module.exports = content.locals;
-	// Hot Module Replacement
-	if(false) {
-		// When the styles change, update the <style> tags
-		if(!content.locals) {
-			module.hot.accept("!!./../node_modules/css-loader/index.js!./stylesheet.css", function() {
-				var newContent = require("!!./../node_modules/css-loader/index.js!./stylesheet.css");
-				if(typeof newContent === 'string') newContent = [[module.id, newContent, '']];
-				update(newContent);
-			});
-		}
-		// When the module is disposed, remove the <style> tags
-		module.hot.dispose(function() { update(); });
-	}
-
-/***/ },
-/* 2 */
-/***/ function(module, exports, __webpack_require__) {
-
-	exports = module.exports = __webpack_require__(3)();
-	// imports
-
-
-	// module
-	exports.push([module.id, ".mjx-cursor {\r\n  -webkit-animation: 1s mj-ed-blink step-end infinite;\r\n  -moz-animation: 1s mj-ed-blink step-end infinite;\r\n  -ms-animation: 1s mj-ed-blink step-end infinite;\r\n  -o-animation: 1s mj-ed-blink step-end infinite;\r\n  animation: 1s mj-ed-blink step-end infinite;\r\n  border-right: 2px solid #000;\r\n  color: transparent;\r\n}\r\n\r\n.mjx-cursor.wasRecentlyPlaced {\r\n  border-right-color: black !important;\r\n}\r\n\r\n.mj-ed-input {\r\n  left: -100%;\r\n  position: absolute;\r\n  top: -100%;\r\n}\r\n\r\n.mj-ed-display {\r\n  box-sizing: border-box;\r\n}\r\n\r\n.mj-ed-display * {\r\n  outline: none;\r\n}\r\n\r\n.mj-ed-selectionButton {\r\n  cursor: text;\r\n}\r\n\r\n.mjx-isEmpty {\r\n  color: #ccc;\r\n}\r\n\r\n@keyframes mj-ed-blink {\r\n  from, to {\r\n    border-color: black;\r\n  }\r\n  50% {\r\n    border-color: transparent;\r\n  }\r\n}\r\n\r\n@-moz-keyframes mj-ed-blink {\r\n  from, to {\r\n    border-color: transparent;\r\n  }\r\n  50% {\r\n    border-color: black;\r\n  }\r\n}\r\n\r\n@-webkit-keyframes mj-ed-blink {\r\n  from, to {\r\n    border-color: transparent;\r\n  }\r\n  50% {\r\n    border-color: black;\r\n  }\r\n}\r\n\r\n@-ms-keyframes mj-ed-blink {\r\n  from, to {\r\n    border-color: transparent;\r\n  }\r\n  50% {\r\n    border-color: black;\r\n  }\r\n}\r\n\r\n@-o-keyframes mj-ed-blink {\r\n  from, to {\r\n    border-color: transparent;\r\n  }\r\n  50% {\r\n    border-color: black;\r\n  }\r\n}", ""]);
-
-	// exports
-
-
-/***/ },
-/* 3 */
-/***/ function(module, exports) {
-
-	/*
-		MIT License http://www.opensource.org/licenses/mit-license.php
-		Author Tobias Koppers @sokra
-	*/
-	// css base code, injected by the css-loader
-	module.exports = function() {
-		var list = [];
-
-		// return the list of modules as css string
-		list.toString = function toString() {
-			var result = [];
-			for(var i = 0; i < this.length; i++) {
-				var item = this[i];
-				if(item[2]) {
-					result.push("@media " + item[2] + "{" + item[1] + "}");
-				} else {
-					result.push(item[1]);
-				}
-			}
-			return result.join("");
-		};
-
-		// import a list of modules into the list
-		list.i = function(modules, mediaQuery) {
-			if(typeof modules === "string")
-				modules = [[null, modules, ""]];
-			var alreadyImportedModules = {};
-			for(var i = 0; i < this.length; i++) {
-				var id = this[i][0];
-				if(typeof id === "number")
-					alreadyImportedModules[id] = true;
-			}
-			for(i = 0; i < modules.length; i++) {
-				var item = modules[i];
-				// skip already imported module
-				// this implementation is not 100% perfect for weird media query combinations
-				//  when a module is imported multiple times with different media queries.
-				//  I hope this will never occur (Hey this way we have smaller bundles)
-				if(typeof item[0] !== "number" || !alreadyImportedModules[item[0]]) {
-					if(mediaQuery && !item[2]) {
-						item[2] = mediaQuery;
-					} else if(mediaQuery) {
-						item[2] = "(" + item[2] + ") and (" + mediaQuery + ")";
-					}
-					list.push(item);
-				}
-			}
-		};
-		return list;
-	};
-
-
-/***/ },
-/* 4 */
-/***/ function(module, exports, __webpack_require__) {
-
-	/*
-		MIT License http://www.opensource.org/licenses/mit-license.php
-		Author Tobias Koppers @sokra
-	*/
-	var stylesInDom = {},
-		memoize = function(fn) {
-			var memo;
-			return function () {
-				if (typeof memo === "undefined") memo = fn.apply(this, arguments);
-				return memo;
-			};
-		},
-		isOldIE = memoize(function() {
-			return /msie [6-9]\b/.test(window.navigator.userAgent.toLowerCase());
-		}),
-		getHeadElement = memoize(function () {
-			return document.head || document.getElementsByTagName("head")[0];
-		}),
-		singletonElement = null,
-		singletonCounter = 0,
-		styleElementsInsertedAtTop = [];
-
-	module.exports = function(list, options) {
-		if(false) {
-			if(typeof document !== "object") throw new Error("The style-loader cannot be used in a non-browser environment");
-		}
-
-		options = options || {};
-		// Force single-tag solution on IE6-9, which has a hard limit on the # of <style>
-		// tags it will allow on a page
-		if (typeof options.singleton === "undefined") options.singleton = isOldIE();
-
-		// By default, add <style> tags to the bottom of <head>.
-		if (typeof options.insertAt === "undefined") options.insertAt = "bottom";
-
-		var styles = listToStyles(list);
-		addStylesToDom(styles, options);
-
-		return function update(newList) {
-			var mayRemove = [];
-			for(var i = 0; i < styles.length; i++) {
-				var item = styles[i];
-				var domStyle = stylesInDom[item.id];
-				domStyle.refs--;
-				mayRemove.push(domStyle);
-			}
-			if(newList) {
-				var newStyles = listToStyles(newList);
-				addStylesToDom(newStyles, options);
-			}
-			for(var i = 0; i < mayRemove.length; i++) {
-				var domStyle = mayRemove[i];
-				if(domStyle.refs === 0) {
-					for(var j = 0; j < domStyle.parts.length; j++)
-						domStyle.parts[j]();
-					delete stylesInDom[domStyle.id];
-				}
-			}
-		};
-	}
-
-	function addStylesToDom(styles, options) {
-		for(var i = 0; i < styles.length; i++) {
-			var item = styles[i];
-			var domStyle = stylesInDom[item.id];
-			if(domStyle) {
-				domStyle.refs++;
-				for(var j = 0; j < domStyle.parts.length; j++) {
-					domStyle.parts[j](item.parts[j]);
-				}
-				for(; j < item.parts.length; j++) {
-					domStyle.parts.push(addStyle(item.parts[j], options));
-				}
-			} else {
-				var parts = [];
-				for(var j = 0; j < item.parts.length; j++) {
-					parts.push(addStyle(item.parts[j], options));
-				}
-				stylesInDom[item.id] = {id: item.id, refs: 1, parts: parts};
-			}
-		}
-	}
-
-	function listToStyles(list) {
-		var styles = [];
-		var newStyles = {};
-		for(var i = 0; i < list.length; i++) {
-			var item = list[i];
-			var id = item[0];
-			var css = item[1];
-			var media = item[2];
-			var sourceMap = item[3];
-			var part = {css: css, media: media, sourceMap: sourceMap};
-			if(!newStyles[id])
-				styles.push(newStyles[id] = {id: id, parts: [part]});
-			else
-				newStyles[id].parts.push(part);
-		}
-		return styles;
-	}
-
-	function insertStyleElement(options, styleElement) {
-		var head = getHeadElement();
-		var lastStyleElementInsertedAtTop = styleElementsInsertedAtTop[styleElementsInsertedAtTop.length - 1];
-		if (options.insertAt === "top") {
-			if(!lastStyleElementInsertedAtTop) {
-				head.insertBefore(styleElement, head.firstChild);
-			} else if(lastStyleElementInsertedAtTop.nextSibling) {
-				head.insertBefore(styleElement, lastStyleElementInsertedAtTop.nextSibling);
-			} else {
-				head.appendChild(styleElement);
-			}
-			styleElementsInsertedAtTop.push(styleElement);
-		} else if (options.insertAt === "bottom") {
-			head.appendChild(styleElement);
-		} else {
-			throw new Error("Invalid value for parameter 'insertAt'. Must be 'top' or 'bottom'.");
-		}
-	}
-
-	function removeStyleElement(styleElement) {
-		styleElement.parentNode.removeChild(styleElement);
-		var idx = styleElementsInsertedAtTop.indexOf(styleElement);
-		if(idx >= 0) {
-			styleElementsInsertedAtTop.splice(idx, 1);
-		}
-	}
-
-	function createStyleElement(options) {
-		var styleElement = document.createElement("style");
-		styleElement.type = "text/css";
-		insertStyleElement(options, styleElement);
-		return styleElement;
-	}
-
-	function createLinkElement(options) {
-		var linkElement = document.createElement("link");
-		linkElement.rel = "stylesheet";
-		insertStyleElement(options, linkElement);
-		return linkElement;
-	}
-
-	function addStyle(obj, options) {
-		var styleElement, update, remove;
-
-		if (options.singleton) {
-			var styleIndex = singletonCounter++;
-			styleElement = singletonElement || (singletonElement = createStyleElement(options));
-			update = applyToSingletonTag.bind(null, styleElement, styleIndex, false);
-			remove = applyToSingletonTag.bind(null, styleElement, styleIndex, true);
-		} else if(obj.sourceMap &&
-			typeof URL === "function" &&
-			typeof URL.createObjectURL === "function" &&
-			typeof URL.revokeObjectURL === "function" &&
-			typeof Blob === "function" &&
-			typeof btoa === "function") {
-			styleElement = createLinkElement(options);
-			update = updateLink.bind(null, styleElement);
-			remove = function() {
-				removeStyleElement(styleElement);
-				if(styleElement.href)
-					URL.revokeObjectURL(styleElement.href);
-			};
-		} else {
-			styleElement = createStyleElement(options);
-			update = applyToTag.bind(null, styleElement);
-			remove = function() {
-				removeStyleElement(styleElement);
-			};
-		}
-
-		update(obj);
-
-		return function updateStyle(newObj) {
-			if(newObj) {
-				if(newObj.css === obj.css && newObj.media === obj.media && newObj.sourceMap === obj.sourceMap)
-					return;
-				update(obj = newObj);
-			} else {
-				remove();
-			}
-		};
-	}
-
-	var replaceText = (function () {
-		var textStore = [];
-
-		return function (index, replacement) {
-			textStore[index] = replacement;
-			return textStore.filter(Boolean).join('\n');
-		};
-	})();
-
-	function applyToSingletonTag(styleElement, index, remove, obj) {
-		var css = remove ? "" : obj.css;
-
-		if (styleElement.styleSheet) {
-			styleElement.styleSheet.cssText = replaceText(index, css);
-		} else {
-			var cssNode = document.createTextNode(css);
-			var childNodes = styleElement.childNodes;
-			if (childNodes[index]) styleElement.removeChild(childNodes[index]);
-			if (childNodes.length) {
-				styleElement.insertBefore(cssNode, childNodes[index]);
-			} else {
-				styleElement.appendChild(cssNode);
-			}
-		}
-	}
-
-	function applyToTag(styleElement, obj) {
-		var css = obj.css;
-		var media = obj.media;
-
-		if(media) {
-			styleElement.setAttribute("media", media)
-		}
-
-		if(styleElement.styleSheet) {
-			styleElement.styleSheet.cssText = css;
-		} else {
-			while(styleElement.firstChild) {
-				styleElement.removeChild(styleElement.firstChild);
-			}
-			styleElement.appendChild(document.createTextNode(css));
-		}
-	}
-
-	function updateLink(linkElement, obj) {
-		var css = obj.css;
-		var sourceMap = obj.sourceMap;
-
-		if(sourceMap) {
-			// http://stackoverflow.com/a/26603875
-			css += "\n/*# sourceMappingURL=data:application/json;base64," + btoa(unescape(encodeURIComponent(JSON.stringify(sourceMap)))) + " */";
-		}
-
-		var blob = new Blob([css], { type: "text/css" });
-
-		var oldSrc = linkElement.href;
-
-		linkElement.href = URL.createObjectURL(blob);
-
-		if(oldSrc)
-			URL.revokeObjectURL(oldSrc);
-	}
-
-
-/***/ },
-/* 5 */
-/***/ function(module, exports, __webpack_require__) {
-
 	'use strict';
 
 	Object.defineProperty(exports, "__esModule", {
@@ -608,19 +258,19 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-	var _EventBus = __webpack_require__(6);
+	var _EventBus = __webpack_require__(2);
 
 	var _EventBus2 = _interopRequireDefault(_EventBus);
 
-	var _Placer = __webpack_require__(7);
+	var _Placer = __webpack_require__(3);
 
 	var _Placer2 = _interopRequireDefault(_Placer);
 
-	var _Iterator = __webpack_require__(8);
+	var _Iterator = __webpack_require__(4);
 
 	var _Iterator2 = _interopRequireDefault(_Iterator);
 
-	var _utils = __webpack_require__(9);
+	var _utils = __webpack_require__(5);
 
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -1274,7 +924,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	exports.default = Editor;
 
 /***/ },
-/* 6 */
+/* 2 */
 /***/ function(module, exports) {
 
 	"use strict";
@@ -1307,7 +957,6 @@ return /******/ (function(modules) { // webpackBootstrap
 	      }
 
 	      if (this.registry[type]) {
-	        console.log(this.registry);
 	        this.registry[type].forEach(function (listener) {
 	          return listener.apply(undefined, rest);
 	        });
@@ -1321,7 +970,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	exports.default = EventBus;
 
 /***/ },
-/* 7 */
+/* 3 */
 /***/ function(module, exports) {
 
 	'use strict';
@@ -1786,7 +1435,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	exports.default = Placer;
 
 /***/ },
-/* 8 */
+/* 4 */
 /***/ function(module, exports) {
 
 	"use strict";
@@ -2090,7 +1739,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	exports.default = Iterator;
 
 /***/ },
-/* 9 */
+/* 5 */
 /***/ function(module, exports) {
 
 	'use strict';
@@ -2215,8 +1864,8 @@ return /******/ (function(modules) { // webpackBootstrap
 	}
 
 /***/ },
-/* 10 */
-/***/ function(module, exports) {
+/* 6 */
+/***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
 
@@ -2224,6 +1873,13 @@ return /******/ (function(modules) { // webpackBootstrap
 	  value: true
 	});
 	exports.default = extendMathJax;
+
+	var _styles = __webpack_require__(7);
+
+	var _styles2 = _interopRequireDefault(_styles);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 	/**
 	 * This will extend MathJax so that we can put our simple
 	 * cursor there.
@@ -2234,6 +1890,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	  // This removes the pause (in milliseconds) between input and output 
 	  // phases of MathJax's processing. So it looks seamless!
+
 	  MathJax.Hub.processSectionDelay = 0;
 
 	  MathJax.Hub.Register.StartupHook("TeX Jax Ready", function () {
@@ -2283,7 +1940,65 @@ return /******/ (function(modules) { // webpackBootstrap
 	      }
 	    });
 	  });
+
+	  MathJax.Ajax.Styles(_styles2.default);
 	}
+
+/***/ },
+/* 7 */
+/***/ function(module, exports) {
+
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+
+	var _mjxCursor$MjxCur;
+
+	function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+	var animation = 'from, to { border-color: #000 }\n 50% { border-color: transparent }';
+
+	exports.default = (_mjxCursor$MjxCur = {
+	  '.mjx-cursor': {
+	    '-webkit-animation': '1s mj-ed-blink step-end infinite',
+	    '-moz-animation': '1s mj-ed-blink step-end infinite',
+	    '-ms-animation': '1s mj-ed-blink step-end infinite',
+	    '-o-animation': '1s mj-ed-blink step-end infinite',
+	    animation: '1s mj-ed-blink step-end infinite',
+	    'border-right': '2px solid #000',
+	    color: 'transparent'
+	  },
+
+	  '.mjx-cursor.wasRecentlyPlaced': {
+	    'border-right-color': '#000 !important'
+	  },
+
+	  '.mj-ed-input': {
+	    left: '-100%',
+	    position: 'absolute',
+	    top: '-100%'
+	  },
+
+	  '.mj-ed-display': {
+	    'box-sizing': 'border-box'
+	  },
+
+	  '.mj-ed-display *': {
+	    outline: 'none'
+	  },
+
+	  '.mj-ed-selectionButton': {
+	    cursor: 'text'
+	  },
+
+	  '.mjx-isEmpty': {
+	    color: '#ccc'
+	  },
+
+	  '@keyframes mj-ed-blink': animation
+	}, _defineProperty(_mjxCursor$MjxCur, '@keyframes mj-ed-blink', animation), _defineProperty(_mjxCursor$MjxCur, '@-moz-keyframes mj-ed-blink', animation), _defineProperty(_mjxCursor$MjxCur, '@-webkit-keyframes mj-ed-blink', animation), _defineProperty(_mjxCursor$MjxCur, '@-ms-keyframes mj-ed-blink', animation), _defineProperty(_mjxCursor$MjxCur, '@-o-keyframes mj-ed-blink', animation), _mjxCursor$MjxCur);
 
 /***/ }
 /******/ ])
