7c6501b612d22d9ebb44256a13097031f92da7fa
Ian Lucas <ianlucas_ferreira@hotmail.com>
2017-08-08 17:16:09 -0300

    Add insertVariable method

--- src/math-jax-editor.js
+++ src/math-jax-editor.js
@@ -22,4 +22,29 @@ export default class MathJaxEditor {
 
     this.editor.insert($mn)
   }
+
+  /**
+   * @param {String} i
+   */
+  insertVariable(i) {
+    if (typeof i !== 'string' && !i.match(/^[a-zA-Z]$/)) {
+      throw new RangeError('MathjaxEditor: A single letter must be provided.')
+    }
+
+    const $mi = document.createElement('mi')
+    $mi.innerHTML = i
+
+    this.editor.insert($mi)
+  }
+
+  insertFraction() {
+    const $mfrac = document.createElement('mfrac')
+    const $mrowNum = document.createElement('mrow')
+    const $mrowDen = document.createElement('mrow')
+
+    $mfrac.appendChild($mrowNum)
+    $mfrac.appendChild($mrowDen)
+
+    this.editor.insert($mfrac)
+  }
 }
