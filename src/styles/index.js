60a79caeeaf70d5e156ca69fdf944dcaf0e90b43
Ian Lucas <ianlucas_ferreira@hotmail.com>
2017-08-09 13:49:18 -0300

    Add file with default styles of the editor

---
+++ src/styles/index.js
@@ -0,0 +1,22 @@
+export default {
+  '.mathjax-editor-input': {
+    'left': '-100%',
+    'position': 'absolute',
+    'top': '-100%'
+  },
+
+  '.mathjax-editor-display': {
+    'cursor': 'text',
+    'position': 'relative'
+  },
+
+  '.mathjax-editor-display .mjx-chtml': {
+    'outline': 'none !important'
+  },
+
+  '.mathjax-editor-cursor': {
+    'background-color': '#000',
+    'position': 'absolute',
+    'width': '1px'
+  }
+}
--- src/utils/extend-math-jax.js
+++ src/utils/extend-math-jax.js
@@ -1,7 +1,10 @@
+import styles from '../styles'
+
 window.addEventListener('load', () => {
   if (!MathJax) {
     throw new Error('MathjaxEditor: MathJax is missing.')
   }
   
   MathJax.Hub.processSectionDelay = 0;
+  MathJax.Ajax.Styles(styles)
 })
