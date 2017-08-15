e589d8f0d2613599dbc3d2b3cd25acc1cd1e95bd
Ian Lucas <ianlucas_ferreira@hotmail.com>
2017-08-15 16:37:39 -0300

    Add test for mml2Tex

--- test/mml2tex.test.js
+++ test/mml2tex.test.js
@@ -1,18 +1,23 @@
 import loadHtml from './load-html'
 import mml2Tex from '../src/mml2tex'
 
+const findX = loadHtml('./find-x.html')
 const gravityFormula = loadHtml('./gravity-formula.html')
 const massEnergyEquivalence = loadHtml('./mass-energy-equivalence.html')
 const quadraticFormula = loadHtml('./quadratic-formula.html')
 
-test('Quadratic formula', () => {
+test('Quadratic formula (frac, sup, sqrt)', () => {
   expect(mml2Tex(quadraticFormula)).toBe('x=\\frac{-b\\pm\\sqrt{{b}^{2}-4ac}}{2a}')
 })
 
-test('Mass-Energy equivalence', () => {
+test('Mass-Energy equivalence (sup)', () => {
   expect(mml2Tex(massEnergyEquivalence)).toBe('e=m{c}^{2}')
 })
 
-test('Gravity formula', () => {
+test('Gravity formula (frac, sup, sub)', () => {
   expect(mml2Tex(gravityFormula)).toBe('F=\\frac{G{m}_{1}{m}_{2}}{{d}^{2}}')
+})
+
+test('Find X (frac, newline)', () => {
+  expect(mml2Tex(findX)).toBe('2x+10=20\\\\2x=20-10\\\\2x=10\\\\x=\\frac{10}{2}\\\\x=5')
 })
