dbd4ac7d10c392aab01dee05399ec433bc903f26
Ian Lucas <ianlucas_ferreira@hotmail.com>
2017-08-15 23:04:21 -0300

    Reuse util function

--- test/load-html.js
+++ test/load-html.js
@@ -1,8 +1,7 @@
 import { readFileSync } from 'fs'
 import { resolve } from 'path'
+import toDom from '../src/utils/to-dom'
 
 export default function loadHtml(file) {
-  const tmp = document.createElement('div')
-  tmp.innerHTML = readFileSync(resolve(__dirname, file), 'utf-8')
-  return tmp.firstElementChild
+  return toDom(readFileSync(resolve(__dirname, file), 'utf-8'))
 }
