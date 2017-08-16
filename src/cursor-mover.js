6e943e3ee1cbf842126f65f96d6e788edb5ac712
Ian Lucas <ianlucas_ferreira@hotmail.com>
2017-08-15 21:12:59 -0300

    Document CursorMover class

--- src/cursor-mover.js
+++ src/cursor-mover.js
@@ -1,4 +1,14 @@
 export default class CursorMover {
+  /**
+   * This class handles where the cursor should be placed
+   * when the user clicks the display.
+   * 
+   * @param {Tree} tree
+   * @param {Rendered} rendered
+   * @param {Cursor} cursor
+   * 
+   * @constructor
+   */
   constructor(tree, rendered, cursor) {
     /** @type {Tree} */
     this.tree = tree
@@ -8,9 +18,22 @@ export default class CursorMover {
     this.cursor = cursor
   }
 
+  /**
+   * Perform the calculation to determine where the cursor
+   * should be placed.
+   * 
+   * @param {Number} x
+   * @param {Number} y
+   * 
+   * @return {Void}
+   */
   click(x, y) {
     let shortest = Infinity
     let $set = null
+
+    // Here we check if the (x, y) is outside the boundings
+    // of the lines, so the cursor can be placed at the first element
+    // or last element of a line.
 
     for (let line of this.rendered.getLines()) {
       if (!line.betweenYAxis(y)) {continue}
@@ -27,6 +50,15 @@ export default class CursorMover {
         }
       }
     }
+
+    // Dunno how much expensive getBoundingClientRect is, but
+    // this seems to be the best solution I had to deal with
+    // proper cursor placement without doing tricky code.
+
+    // We basically set the cursor to every possible place it
+    // can be at, then we calculate its distance to the given
+    // x and y. The shortest to that coordinate will determine
+    // where it should be placed.
 
     for (const $el of this.tree.getPath()) {
       if ($el && $el.tagName === 'MATH') {continue}
