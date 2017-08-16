342d9a04a20925dc2e31c9efec291a87b8284203
Ian Lucas <ianlucas_ferreira@hotmail.com>
2017-08-15 22:28:42 -0300

    Add argument for attributes

--- src/utils/create-element.js
+++ src/utils/create-element.js
@@ -2,12 +2,22 @@
  * Quickly create an element with className.
  * 
  * @param {String} tagName 
- * @param {String} className 
+ * @param {String} [className=''] 
+ * @param {Object} [attributes={}]
  * 
  * @return {HTMLElement}
  */
-export default function createElement(tagName, className = '') {
+export default function createElement(tagName, className = '', attributes = {}) {
+  if (typeof className === 'object') {
+    attributes = className
+    className = ''
+  }
+
   const $el = document.createElement(tagName)
   $el.className = className
+
+  Object.keys(attributes).forEach(key => {
+    $el.setAttribute(key, attributes[key])
+  })
   return $el
 }
